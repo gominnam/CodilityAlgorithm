90db6c23e68292a799bf8a858327fd17ae5ded3b
ko <kosok03@nate.com>
2021-06-19 00:47:39 +0900

    lesson3 [PermMissingElem]

--- src/TimeComplexity/FrogJmp.java
+++ src/TimeComplexity/FrogJmp.java
@@ -9,3 +9,10 @@ public class FrogJmp {
         return answer;
     }
 }
+
+/* 피드백
+1. 시간복잡도를 고려하자
+
+결론: 처음에 그냥 do~while 문으로 O(N)의 복잡도를 사용해서 틀렸다.
+     더 효율적인 O(1)의 방법: 나눗셈을 사용하여 pass..
+ */
--- src/TimeComplexity/PermMissingElem.java
+++ src/TimeComplexity/PermMissingElem.java
@@ -13,3 +13,9 @@ public class PermMissingElem {
         return index;
     }
 }
+
+/* 피드백
+
+결론: 별다른 문제점 없었다. 정렬이 다 됐을 경우에 마지막 return 값 조건이 없어서 0을 넣어서 틀렸다.
+      index를 return해주니까 pass 했다.
+ */
