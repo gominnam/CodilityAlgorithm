5e5669c411d418d9ee84d9e8e7f04823193cf58b
gominnam <kosok03@naver.com>
2021-07-15 01:07:09 +0900

    Inflearn - KThBigNumber [TreeSet]

---
+++ src/Inflearn/HashMapAndTreeSet/KThBigNumber.java
@@ -0,0 +1,68 @@
+package Inflearn.HashMapAndTreeSet;
+
+import java.util.*;
+
+public class KThBigNumber {
+    public int Solve(int n, int m, int[] arr){
+        int answer = -1;
+        TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    ts.add(arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int x : ts){
+            cnt++;
+            if(cnt == m) {
+                return x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        KThBigNumber T = new KThBigNumber();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(T.Solve(n, m, arr));
+    }
+}
+
+/*
+remind
+설명
+
+현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 같은 숫자의 카드가 여러장 있을 수 있습니다.
+
+현수는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려고 합니다. 3장을 뽑을 수 있는 모든 경우를 기록합니다.
+
+기록한 값 중 K번째로 큰 수를 출력하는 프로그램을 작성하세요.
+
+만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값은 22입니다.
+
+
+입력
+첫 줄에 자연수 N(3<=N<=100)과 K(1<=K<=50) 입력되고, 그 다음 줄에 N개의 카드값이 입력된다.
+
+
+출력
+첫 줄에 K번째 수를 출력합니다. K번째 수가 존재하지 않으면 -1를 출력합니다.
+
+TEST CASE:
+10 3
+13 15 34 23 45 65 33 11 26 42
+
+==> 143
+ */
