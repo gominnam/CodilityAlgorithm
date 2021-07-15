a2f36e867555a934e2058c3e3b47eb98f63b75c1
gominnam <kosok03@naver.com>
2021-07-15 15:10:49 +0900

    Programmers NewIdRecommend - String 문제

---
+++ src/Programmers/Level1/NewIdRecommend.java
@@ -0,0 +1,26 @@
+package Programmers.Level1;
+
+import java.util.*;
+
+public class NewIdRecommend {
+    public String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase();
+        for(int i=0; i<new_id.length(); i++){
+            if(new_id.charAt(i) == '!' || new_id.charAt(i) == '@' || new_id.charAt(i) == '#'){
+                new_id.replace('!', '');
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        NewIdRecommend T = new NewIdRecommend();
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(T.solution(s));
+    }
+}
