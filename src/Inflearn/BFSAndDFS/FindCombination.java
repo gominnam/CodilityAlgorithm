1cf7c620a9db81798039fc69f9366dc9ccdde8b8
gominnam <kosok03@naver.com>
2021-08-01 22:38:40 +0900

    Inflearn - FindCombination

---
+++ src/Inflearn/BFSAndDFS/FindCombination.java
@@ -0,0 +1,45 @@
+package Inflearn.BFSAndDFS;
+
+import java.util.Scanner;
+
+public class FindCombination {
+    static int[] combi;
+    static int n, m;
+
+    public void DFS(int L, int s){// L: 레벨, S: 스타트 번호
+        if(L==m){
+            for(int x : combi) System.out.print(x + " ");
+            System.out.println();
+        }
+        else{
+            for(int i=s; i<=n; i++){
+                combi[L] = i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        FindCombination T = new FindCombination();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        combi = new int[m];
+        T.DFS(0, 1);
+    }
+}
+/*
+조합 구하기
+
+TEST CASE:
+4 2
+
+==>
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+
+ */
