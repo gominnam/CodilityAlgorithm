8c6bf0ea73be6899d9392023627b44c39a568a0e
gominnam <kosok03@naver.com>
2021-08-18 22:51:26 +0900

    Inflearn - MaximumIncome(Greedy)

---
+++ src/Inflearn/Greedy/MaximumIncome.java
@@ -0,0 +1,80 @@
+package Inflearn.Greedy;
+
+import java.util.*;
+
+public class MaximumIncome {
+
+    public static class Schedule implements Comparable<Schedule>{
+        int pay;
+        int day;
+        public Schedule(int p, int d){
+            this.pay = p;
+            this.day = d;
+        }
+        @Override
+        public int compareTo(Schedule s){
+            if(this.pay == s.pay) return this.day - s.day;
+            return s.pay - this.pay;
+        }
+    }
+
+    public int solution(ArrayList<Schedule> arr, int day){
+        Collections.sort(arr);
+        int income = 0, curD = 0;
+        for(Schedule s : arr){
+            if(day == curD) return income;
+            if(s.day > curD){
+                income += s.pay;
+                curD++;
+            }
+        }
+        return income;
+    }
+
+    public static void main(String[] args){
+        MaximumIncome T = new MaximumIncome();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Schedule> arr = new ArrayList<>();
+        int n = sc.nextInt();
+        int maxD = 0;
+        for(int i=0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr.add(new Schedule(a, b));
+            maxD = Math.max(maxD, b);
+        }
+
+        System.out.println(T.solution(arr, maxD));
+    }
+}
+
+/*
+설명
+
+현수는 유명한 강연자이다. N개이 기업에서 강연 요청을 해왔다. 각 기업은 D일 안에 와서 강연을 해 주면 M만큼의 강연료를 주기로 했다.
+
+각 기업이 요청한 D와 M를 바탕으로 가장 많을 돈을 벌 수 있도록 강연 스케쥴을 짜야 한다.
+
+단 강연의 특성상 현수는 하루에 하나의 기업에서만 강연을 할 수 있다.
+
+
+입력
+첫 번째 줄에 자연수 N(1<=N<=10,000)이 주어지고, 다음 N개의 줄에 M(1<=M<=10,000)과 D(1<=D<=10,000)가 차례로 주어진다.
+
+
+출력
+첫 번째 줄에 최대로 벌 수 있는 수입을 출력한다.
+
+TEST CASE:
+6
+50 2
+20 1
+40 2
+60 3
+30 3
+30 1
+
+
+==> 150
+
+ */
