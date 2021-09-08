27533eb14360589108f4c69e8b460d7524c5267b
gominnam <kosok03@naver.com>
2021-09-08 21:21:37 +0900

    Inflearn - String Basic Grammer2

--- src/Inflearn/Array/Eratos.java
+++ src/Inflearn/Array/Eratos.java
@@ -3,22 +3,18 @@ package Inflearn.Array;
 import java.util.Scanner;
 
 public class Eratos {
-    public int Solve(int n){
+    public int Solve(int n) {
         int answer = 0;
-
-        for(int i=2; i<=n; i++){
-            if(isPrime(i)) answer++;
+        int[] ch = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
         }
-
         return answer;
-    }
-
-    public boolean isPrime(int n){
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i==0)  return false;
-        }
-
-        return true;
     }
 
     public static void main(String[] args){
@@ -32,18 +28,22 @@ public class Eratos {
 }
 
 /*
+설명
 
-현재의 수를 소수인지 확인하고 그 값의 배수를 flag를 주는 것을 에라토스의 체라고 한다.
-public int solution(int n) {
-    int answer = 0;
-    int[] ch = new int[n + 1];
-    for(int i=2; i<=n; i++){
-        if(ch[i]==0){
-            answer++;
-            for(int j=i; j<=n; j=j+i) ch[j] = 1;
-        }
-    }
-    return answer;
-}
+자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
 
+만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+
+
+입력
+첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+
+
+출력
+첫 줄에 소수의 개수를 출력합니다.
+
+TEST CASE:
+20
+
+==> 8
  */
---
+++ src/Inflearn/String/ShortestWordDistance.java
@@ -0,0 +1,65 @@
+package Inflearn.String;
+
+import java.util.*;
+
+public class ShortestWordDistance {
+    public void solution(String str, char c){
+        int[] arr = new int[str.length()];
+        int p = 10000000;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == c){
+                p=0;
+                arr[i] = p++;
+            }
+            else{
+                arr[i] = p++;
+            }
+        }
+
+        p = 10000000;
+        for(int i=str.length()-1; i>=0; i--){
+            if(str.charAt(i) == c) {
+                p = 0;
+                p++;
+            }
+            else{
+                arr[i] = Math.min(arr[i], p++);
+            }
+        }
+
+        for(int i=0; i<str.length();i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        ShortestWordDistance T = new ShortestWordDistance();
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c =sc.next().charAt(0);
+
+        T.solution(str, c);
+    }
+}
+/*
+feedback - 왼쪽에서 n번 오른쪽에서 n번 for문 돌려서 거리 min 값으로 설정한다.
+설명
+
+한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 번째 줄에 문자열 s와 문자 t가 주어진다. 문자열과 문자는 소문자로만 주어집니다.
+
+문자열의 길이는 100을 넘지 않는다.
+
+
+출력
+첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
+
+TEST CASE:
+teachermode e
+
+==> 1 0 1 2 1 0 1 2 2 1 0
+ */
