1415303c2c60927149ccc4f79fc89197d2253e95
gominnam <kosok03@naver.com>
2021-09-29 00:40:58 +0900

    Inflearn - Dijstra Algorithm

---
+++ src/BaekJoon/Etc/Dijkstra_1238.java
@@ -0,0 +1,90 @@
+package BaekJoon.Etc;
+
+import java.io.*;
+import java.util.*;
+
+class Node implements Comparable<Node>{
+    int end;
+    int weight;
+
+    public Node(int end, int weight){
+        this.end = end;
+        this.weight = weight;
+    }
+
+    public int compareTo(Node n){
+        return this.weight - n.weight;
+    }
+}
+
+public class Dijkstra_1238 {
+    static int N, M, X;
+    static ArrayList<ArrayList<Node>> arr = new ArrayList<>();
+    static ArrayList<ArrayList<Node>> reverse_arr = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        for(int i=0; i<=N; i++){
+            arr.add(new ArrayList<>());
+            reverse_arr.add(new ArrayList<>());
+        }
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            arr.get(start).add(new Node(end, weight));
+            reverse_arr.get(end).add(new Node(start, weight));
+        }
+
+        int[] dis1 = dijkstra(arr);
+        int[] dis2 = dijkstra(reverse_arr);
+
+        int max = Integer.MIN_VALUE;
+        for(int i=1; i<=N; i++){
+            max = Math.max(max, dis1[i]+dis2[i]);
+        }
+
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int[] dijkstra(ArrayList<ArrayList<Node>> a){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(X, 0));
+
+        boolean[] visited = new boolean[N+1];
+        int[] dis = new int[N+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[X] = 0;
+
+        while(!pq.isEmpty()){
+            Node n = pq.poll();
+            int cur = n.end;
+
+            if(!visited[cur]){
+                visited[cur] = true;
+
+                for(Node node : a.get(cur)){
+                    if(!visited[node.end] && dis[node.end] > dis[cur] + node.weight){
+                        dis[node.end] = dis[cur] + node.weight;
+                        pq.add(new Node(node.end, dis[node.end]));
+                    }
+                }
+            }
+        }
+        return dis;
+    }
+}
+/*
+        ## 다익스트라
+
+    TIP - 왕복 문제는 정점을 reverse 하여 구하면 최단거리 구할 수 있다.
+ */
---
+++ src/BaekJoon/Etc/Dijkstra_1916.java
@@ -0,0 +1,80 @@
+package BaekJoon.Etc;
+
+import java.io.*;
+import java.util.*;
+
+class Edge implements Comparable<Edge>{
+    int e, w;//end, weight
+
+    public Edge(int e, int w){
+        this.e = e;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Edge e){
+        return this.w - e.w;//this가 앞이면 오름차순(음수)
+    }
+}
+
+public class Dijkstra_1916 {
+    static ArrayList<ArrayList<Edge>> arr;
+    static int[] dis; //최단거리
+    static boolean[] visited; //방문
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        arr = new ArrayList<>();
+        dis = new int[N+1];
+        visited = new boolean[N+1];
+
+        for(int i=0; i<=N; i++){
+            arr.add(new ArrayList<>());
+        }
+
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            arr.get(s).add(new Edge(e, w));
+        }
+        st = new StringTokenizer(br.readLine(), " ");
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        bw.write(dijkstra(start, end) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int dijkstra(int start, int end){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+        dis[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge curEdge = pq.poll();
+            int cur = curEdge.e;
+
+            if (!visited[cur]) {
+                visited[cur] = true;
+
+                for (Edge edge : arr.get(cur)) {
+                    if (!visited[edge.e] && dis[edge.e] > dis[cur] + edge.w) {
+                        dis[edge.e] = dis[cur] + edge.w;
+                        pq.add(new Edge(edge.e, dis[edge.e]));
+                    }
+                }
+            }
+        }
+        return dis[end];
+    }
+}
