52c0153ceb31f4b87f00a8ddcb544cf8a4b7a004
gominnam <kosok03@naver.com>
2021-09-29 19:40:22 +0900

    BaekJoon - KMP_16916

--- src/BaekJoon/Etc/KMP_16916.java
+++ src/BaekJoon/Etc/KMP_16916.java
@@ -1,18 +1,52 @@
 package BaekJoon.Etc;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class KMP_16916 {
+    static String sentence, word;
+    static int[] pi;
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String sentence = br.readLine();
-        String words = br.readLine();
+        sentence = br.readLine();
+        word = br.readLine();
+        pi = new int[word.length()];
+        getPi();
+        KMP();
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
+    public static void getPi(){
+        int j = 0;
+        for(int i=0; i<word.length(); i++){
+            while(j>0 && word.charAt(i) != word.charAt(j)){//맞는 위치가 나올 때까지 j - 1칸의 공통 부분 위치로 이동
+                j = pi[j-1];
+            }
+            if(word.charAt(i) == word.charAt(j)){
+                pi[i] = ++j;//i길이 문자열의 공통 길이는 j의 위치 + 1
+            }
+        }
+    }
 
-
+    public static void KMP(){
+        int j = 0;
+        for(int i=0; i<sentence.length(); i++){
+            while(j>0 && sentence.charAt(i) != word.charAt(j)){//맞는 위치가 나올 때 까지 j - 1칸의 공통부분 위치로 이동
+                j = pi[j-1]; // <-------- 무한루프에 빠짐
+            }
+            if(sentence.charAt(i) == word.charAt(j)){//맞는경우
+                if(j == word.length() - 1){
+                    result = 1;
+                    break;
+                }
+                else ++j;//맞았지만 패턴이 끝나지 않았다면 j를 하나 증가
+            }
+        }
     }
 }
 /*
