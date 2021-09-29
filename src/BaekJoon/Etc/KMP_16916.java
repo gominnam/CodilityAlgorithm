5f29cbac1faab5f975f83df4e1864217dbd37b55
gominnam <kosok03@naver.com>
2021-09-29 19:24:54 +0900

    BaekJoon - KMP_16916

---
+++ src/BaekJoon/Etc/KMP_16916.java
@@ -0,0 +1,21 @@
+package BaekJoon.Etc;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class KMP_16916 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String sentence = br.readLine();
+        String words = br.readLine();
+
+
+
+    }
+}
+/*
+        ## KMP 알고리즘 - 시간복잡도 O(NM) 선형자료구조조
+
+ */
