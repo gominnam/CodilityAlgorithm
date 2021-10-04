6c60da0dd5ff7d4d2365af66b384172d201ad67a
gominnam <kosok03@naver.com>
2021-10-04 21:57:25 +0900

    Programmers - Level2 KeepDistance

--- src/Programmers/Level2/KeepDistance.java
+++ src/Programmers/Level2/KeepDistance.java
@@ -2,29 +2,69 @@ package Programmers.Level2;
 
 import java.util.*;
 
+class Node{
+    int y, x;
+    Node(int y, int x){
+        this.y = y;
+        this.x = x;
+    }
+}
+
 public class KeepDistance {
+    static int[] mx = {0, 0, -1, 1};
+    static int[] my = {-1, 1, 0, 0};
+
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length]; // P:응시자, O:빈테이블, X:파티션
-        for(int i=0; i<places.length; i++){
+        int[] answer = new int[5];
+        for(int i=0; i<5; i++){
             answer[i] = confirm(places[i]);
-            /*
-            for(String x : places[i]){
-                System.out.println(x);
-            }
-            System.out.println("next");
-            */
         }
 
         return answer;
     }
 
-    public int confirm(String[] p){ // 배열(메모리 순서로 들어옴
-        int[][] check = new int[p.length+1][p.length+1];
+    public static int confirm(String[] str){
+        char[][] map = new char[5][5];
+        for(int i=0; i<str.length; i++){
+            for(int j=0; j<str[i].length(); j++){
+                map[i][j] = str[i].charAt(j);
+            }
+        }
 
+        for(int i=0; i<map.length; i++){
+            for(int j=0; j<map.length; j++){
+                if(map[i][j] == 'P') {
+                    if(bfs(map, i, j)) continue;
+                    else return 0;
+                }
+            }
+        }
 
-
-        //맨해튼 |a - b| + |a2 + b2| > 2
         return 1;
+    }
+
+    public static boolean bfs(char[][] map, int y, int x){
+        Queue<Node> q = new LinkedList<>();
+        boolean[][] visited = new boolean[5][5];
+        q.add(new Node(y, x));
+        visited[y][x] = true;
+
+        while(!q.isEmpty()){
+            Node n = q.poll();
+            for(int i=0; i<4; i++){
+                int ny = n.y + my[i];
+                int nx = n.x + mx[i];
+                int mht = Math.abs(ny - y) + Math.abs(nx - x);
+                if(ny < 0 || ny > 4 || nx < 0 || nx > 4 || visited[ny][nx] || mht > 2) continue;
+
+                visited[ny][nx] = true;
+                if(map[ny][nx] == 'P') return false;
+                else if(map[ny][nx] == 'X') continue;
+                else q.add(new Node(ny, nx));
+
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args){
