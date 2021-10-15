b2632d57b5cb9359a42e5b33421bf1b758e8d3f5
gominnam <kosok03@naver.com>
2021-10-15 21:31:26 +0900

    Basic Algorithm - BinarySearch(이진탐색)

---
+++ src/Basic/BinarySearch.java
@@ -0,0 +1,23 @@
+package Basic;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        int[] arr = {17, 28, 43, 67, 88, 92, 100};
+        int ret = binarySearch(arr, 40);
+        System.out.println(ret);
+    }
+
+    public static int binarySearch(int[] arr, int x){
+        int lo = 0, hi = arr.length-1;
+        while(lo + 1 < hi){
+            int mid = (lo + hi) / 2;
+            if(arr[mid] < x) lo = mid;
+            else hi = mid;
+        }
+
+        return arr[hi];
+    }
+}
+/*
+        ## 이진탐색
+ */
