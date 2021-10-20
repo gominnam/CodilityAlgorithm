4265d75945c6b863466c5f0039c5cdb5b915ec07
gominnam <kosok03@naver.com>
2021-10-21 00:25:24 +0900

    Basic - DP, Subsequence

---
+++ src/Basic/DynamicProgramming.java
@@ -0,0 +1,28 @@
+package Basic;
+
+import java.util.Arrays;
+
+public class DynamicProgramming {
+    static int[][] cache = new int[30][30];
+    public static void main(String[] args){
+//        for(int i=0; i<cache.length; i++){//-1로 초기화
+//            Arrays.fill(cache[i], -1);
+//        }
+        bino2(10, 4);
+        System.out.println(cache[10][4]);
+    }
+    public static int bino2(int n, int r){
+        //기저사례
+        if(r==0 || n==r) return 1;
+        if(cache[n][r] != 0){
+            return cache[n][r];
+        }
+        //직접 계산한 뒤 배열에 저장
+        return cache[n][r] = bino2(n-1, r-1) + bino2(n-1, r);
+    }
+}
+/*
+1. 메모제이션 활용(cache) // 중복연산
+2. nCr 조합 ==> nCr = n-1Cr-1 + n-1Cr
+
+ */
---
+++ src/Basic/MaximumSubsequenceSum.java
@@ -0,0 +1,29 @@
+package Basic;
+
+public class MaximumSubsequenceSum {
+    static int MIN = Integer.MIN_VALUE;
+    public static void main(String[] args){
+        int[] arr = {-7, 4, -3, 6, 3, -8, 3, 4};
+        int ret = fastestMaxSum(arr);
+        System.out.println(ret);
+    }
+
+    public static int fastestMaxSum(int[] arr){
+        int N = arr.length, ret = MIN, psum = 0;
+        for(int i=0; i<N; ++i){
+            psum = Math.max(psum, 0) + arr[i];
+            ret = Math.max(psum, ret);
+        }
+        return ret;
+    }
+}
+/*
+    ## 동적계획법 (시간복잡도 : O(N) // 선형)
+
+    1. psum은 음수가될 경우 0으로 초기화
+    2. 다음 arr[i]를 psum과 더함
+    3. 최대 합 변수와 psum을 비교후 갱신
+
+    위 로직 반복
+
+*/
