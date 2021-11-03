b118a0586817cb320c415087c7540859bd29833c
gominnam <kosok03@naver.com>
2021-11-03 21:36:33 +0900

    JongManBook - TrianglePath (Dynamic Programming)

--- src/JongManBook/DivideConquer/TrianglePath.java
+++ src/JongManBook/DivideConquer/TrianglePath.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class TrianglePath {
     static int[][] tri;
+    static int[][] cache = new int[100][100];//memozation
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,13 +21,23 @@ public class TrianglePath {
                     tri[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
-            bw.write(solve(0, 0, N, 0) + "\n");
+            //bw.write(solve(0, 0, N, 0) + "\n");
+            bw.write(solve2(0, 0, N) + "\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
 
+    public static int solve2(int y, int x, int N){
+        if(y==N-1) return tri[y][x];
+        //메모제이션
+        int ret = cache[y][x];
+        if(ret != 0) return ret;
+        return Math.max(solve2(y+1, x, N), solve2(y+1, x+1, N)) + tri[y][x];
+    }
+
+    /* 시간초과 */
     public static int solve(int y, int x, int N, int ret){
         if(y==N) return ret;
 
@@ -34,6 +46,11 @@ public class TrianglePath {
     }
 }
 /*
+feedBack - 일반 완전탐색으로 할 경우 길이가 100인 경우 2^100-1의 경우의 수 가 있으므로 시간초과가 발생한다.
+           메모제이션인 경우에는 길이만큼 전부 메모리를 할당하는 경우 메모리 사용량이 너무 많다.
+           즉, 점화식을 변경해야 하는데 전체 경로의 최대 합을 반환하는 것이 아니라 !!부분 경로의 최대합!!을 반환하는것이 중요 포인트다.
+           -> 거꾸로 계산하면서 올라간다.
+
 TEST CASE:
 2
 5
