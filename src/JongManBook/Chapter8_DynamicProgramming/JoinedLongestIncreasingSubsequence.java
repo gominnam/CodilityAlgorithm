43dbc92d076e388d231406ac030a1ab49ac1dbc2
gominnam <kosok03@naver.com>
2021-11-10 00:28:46 +0900

    JongManBook - JoinedLongestIncreasingSubsequence (Dynamic Programming)

--- src/JongManBook/Chapter8_DynamicProgramming/JoinedLongestIncreasingSubsequence.java
+++ src/JongManBook/Chapter8_DynamicProgramming/JoinedLongestIncreasingSubsequence.java
@@ -4,8 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class JoinedLongestIncreasingSubsequence {
-    static int n, m;
-    static int[] A = new int[100], B = new int[100];
+    static int N, M;
+    static int[] A, B;
+    static int[][] cache;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,25 +15,76 @@ public class JoinedLongestIncreasingSubsequence {
         int testCase = Integer.parseInt(br.readLine());
         for(int i=0; i<testCase; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            int[] arr1 = new int[n];
-            int[] arr2 = new int[m];
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<arr1.length; j++){
-                arr1[j] = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            A = new int[N];
+            B = new int[M];
+            cache = new int[N+1][M+1];
+            for(int j=0; j<N+1; j++){
+                Arrays.fill(cache[j], -1);
             }
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<arr2.length; j++){
-                arr2[j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<A.length; j++){
+                A[j] = Integer.parseInt(st.nextToken());
             }
-            bw.write(solve(arr1, arr2) + "\n");
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<B.length; j++){
+                B[j] = Integer.parseInt(st.nextToken());
+            }
+            bw.write(solve(-1, -1) + "\n");
         }
         bw.flush();
         bw.close();
         br.close();
     }
-    public static int solve(int[] arr1, int[] arr2){
-        return 1;
+    public static int solve(int indexA, int indexB){
+        int ret = cache[indexA+1][indexB+1];
+        if(ret != -1) return ret;
+        //A[indexA], B[indexB]가 이미 존재하므로 2개는 항상 있다.
+        ret = 0;
+        long a = (indexA == -1 ? Long.MIN_VALUE : A[indexA]);
+        long b = (indexB == -1 ? Long.MIN_VALUE : B[indexB]);
+        long maxElement = Math.max(a, b);
+
+        //다음 원소 찾기
+        for(int nextA = indexA+1; nextA < N; nextA++){
+            if(maxElement < A[nextA]){
+                ret = Math.max(ret, solve(nextA, indexB) + 1);
+            }
+        }
+        for(int nextB = indexB+1; nextB < M; nextB++){
+            if(maxElement < B[nextB]){
+                ret = Math.max(ret, solve(indexA, nextB) + 1);
+            }
+        }
+
+        return cache[indexA+1][indexB+1] = ret;
     }
 }
+/*
+TESTCASE:
+4
+3 3
+1 2 4
+3 4 7
+3 3
+1 2 3
+4 5 6
+5 3
+10 20 30 1 2
+10 20 30
+3 3
+1 9 4
+3 4 7
+==>
+5
+6
+5
+5
+
+
+1
+3 3
+1 9 4
+3 4 7
+ */
