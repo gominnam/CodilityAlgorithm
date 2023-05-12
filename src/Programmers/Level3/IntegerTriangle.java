3cb9617e06180f43f7ec98a51f929315de8c17f0
gominnam <kosok03@naver.com>
2023-05-12 14:47:08 +0900

    add basic TreeMap structure and add Level3 Dynamic programming

--- src/Programmers/Level3/IntegerTriangle.java
+++ src/Programmers/Level3/IntegerTriangle.java
@@ -6,23 +6,17 @@ public class IntegerTriangle {
     public int solution(int[][] triangle) {
         int answer = Integer.MIN_VALUE;
         int n = triangle.length;
-        dp = new int[n][n];
-
-        if(n == 1) return triangle[0][0];
-
-        dp[0][0] = triangle[0][0];
-        for(int i=1; i<n; i++){
-            dp[i][0] = dp[i-1][0]+triangle[i][0];
-        }
 
         for(int i=1; i<n; i++){
-            for(int j=1; j<=i; j++){
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j])+triangle[i][j];
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i][i] += triangle[i-1][i-1];
+            for(int j=1; j<i; j++){
+                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
             }
         }
 
         for(int i=0; i<n; i++){
-            answer = Math.max(answer, dp[n-1][i]);
+            answer = Math.max(answer, triangle[n-1][i]);
         }
 
         return answer;
@@ -34,3 +28,10 @@ public class IntegerTriangle {
         System.out.print(it.solution(triangle));
     }
 }
+/*
+feed back:
+
+- dynamic programming algorithm
+
+
+ */
