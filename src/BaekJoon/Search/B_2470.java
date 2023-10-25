4f7e1832a8195bb467fdad1ba0a2f1c44f6775c0
gominnam <kosok03@naver.com>
2023-10-25 22:18:44 +0900

    add using treeSet structure

--- src/BaekJoon/Search/B_2470.java
+++ src/BaekJoon/Search/B_2470.java
@@ -1,2 +1,49 @@
-package BaekJoon.Search;public class B_2470 {
+package BaekJoon.Search;
+
+import java.io.*;
+import java.util.*;
+
+public class B_2470 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        TreeSet<Integer> tree = new TreeSet<>();
+        int n = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(str[i]);
+            tree.add(num);
+            nums[i] = num;
+        }
+        int[] answer = new int[2];
+        int result = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            Integer floor = tree.floor(-nums[i]);
+            if(floor != nums[i] && floor != null) {
+                int len = Math.abs(nums[i] - floor);
+                if(result > len)
+                    result = len;
+                answer[0] = nums[i];
+                answer[1] = floor;
+            }
+        }
+        Arrays.sort(answer);
+        sb.append(answer[0]).append(" ").append(answer[1]);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
+
+/*
+
+ Structure TreeSet Methods
+ 1. higher (return higher value than input)
+ 2. lower (return lower value than input)
+ 3. ceiling (return higher or equal value than input)
+ 4. floor (return lower or equal value than input)
+
+ */
