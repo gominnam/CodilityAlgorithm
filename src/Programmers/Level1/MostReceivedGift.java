4aa3131fde17697c89aa10cf81a03135c3393aa0
gominnam <kosok03@naver.com>
2024-06-21 23:49:42 +0900

    add programmers level1 MostReceivedGift

---
+++ src/Programmers/Level1/MostReceivedGift.java
@@ -0,0 +1,59 @@
+package Programmers.Level1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MostRecivedGift {
+
+    public static void main(String[] args){
+        String[] friends = {"joy", "brad", "alessandro", "conan", "david"};
+        String[] gifts = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
+
+        Map<String, Map<String, Integer>> map = new HashMap<>();
+        for(String friend : friends){
+            map.put(friend, new HashMap<>());
+        }
+        for(String gift : gifts){
+            String[] split = gift.split(" ");
+            String giver = split[0];
+            String receiver = split[1];
+            map.get(receiver).put(giver, map.get(receiver).getOrDefault(giver, 0) + 1);
+            map.get(receiver).put(receiver, map.get(receiver).getOrDefault(receiver, 0) - 1);
+            map.get(giver).put(giver, map.get(giver).getOrDefault(giver, 0) + 1);
+        }
+
+        int answer = 0;
+        for(String friend : friends){
+            int received = 0;
+            int sent = map.get(friend).getOrDefault(friend, 0);
+            for(String key : map.get(friend).keySet()){
+                if(key.equals(friend)) continue;
+                if(Objects.equals(map.get(friend).get(key), map.get(key).get(key))){
+                    if(sent > map.get(key).get(key)) received++;
+                }
+                else{
+                    if(map.get(friend).getOrDefault(key, 0) > map.get(key).getOrDefault(friend, 0)) received++;
+                }
+            }
+
+            answer = Math.max(answer, received);
+        }
+        System.out.println(answer);
+    }
+}
+
+/*
+- ref: https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=java
+
+# 1.
+friend: ["muzi", "ryan", "frodo", "neo"]
+gifts: ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"]
+answer = 2
+
+# 2.
+friend: ["joy", "brad", "alessandro", "conan", "david"]
+gifts: ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]
+answer = 4
+
+ */
