91dc05dbfabd65073cb67423782ed862f7f9c21e
gominnam <kosok03@naver.com>
2024-06-26 05:32:10 +0900

    add level2 bfs and two-pointer

--- src/Programmers/Level2/ConvertNumber.java
+++ src/Programmers/Level2/ConvertNumber.java
@@ -1,2 +1,72 @@
-package Programmers.Level2;public class ConvertNumber {
+package Programmers.Level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConvertNumber {
+
+    public static int convertNumber(int x, int y, int n) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[1000001];
+        queue.offer(new int[]{x, 0});
+        visited[x] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int number = current[0];
+            int count = current[1];
+
+            if (number == y) {
+                return count;
+            }
+
+            if (count > y) {
+                continue;
+            }
+
+            int[] nextNumbers = {number + n, number * 2, number * 3};
+
+            for (int nextNumber : nextNumbers) {
+                if (nextNumber <= 1000000 && !visited[nextNumber]) {
+                    queue.offer(new int[]{nextNumber, count + 1});
+                    visited[nextNumber] = true;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int x = 38;
+        int y = 40;
+        int n = 2;
+        int answer = convertNumber(x, y, n);
+        System.out.println(answer);
+//       1. x에 n을 더합니다.
+//       2. x에 2를 곱합니다.
+//       3.  x에 3을 곱합니다.
+    }
 }
+
+/*
+
+-ref: https://school.programmers.co.kr/learn/courses/30/lessons/154538
+bfs
+
+TEST CASE:
+1)
+x: 10
+y: 40
+n: 5
+
+answer: 2
+
+2)
+x: 10
+y: 40
+n: 30
+
+answer: 1
+
+ */
--- src/Programmers/Level2/SumConsecutiveSubsequence.java
+++ src/Programmers/Level2/SumConsecutiveSubsequence.java
@@ -1,2 +1,42 @@
-package Programmers.Level2;public class SumConsecutiveSubsequence {
+package Programmers.Level2;
+
+public class SumConsecutiveSubsequence {
+
+    public static void main(String[] args) {
+        int[] sequence = {1, 2, 3, 4, 5};
+        int k = 7;
+        int[] answer = {0, 1000001};
+        int left = 0;
+        for(int right = 0; right < sequence.length; right++) {
+            k -= sequence[right];
+            while(k < 0) {
+                k += sequence[left++];
+            }
+            if(k == 0 && answer[1] - answer[0] > right - left) {
+                answer[0] = left;
+                answer[1] = right;
+            }
+        }
+        System.out.println(answer[0] + " " + answer[1]);
+    }
 }
+
+/*
+- ref: https://school.programmers.co.kr/learn/courses/30/lessons/178870
+two pointer algorithm
+
+TEST CASE:
+
+1)
+sequence = {1, 2, 3, 4, 5}
+k = 7
+
+answer: {2, 3}
+
+2)
+sequence = {1, 1, 1, 2, 3, 4, 5}
+k = 5
+
+answer = {6, 6}
+
+ */
