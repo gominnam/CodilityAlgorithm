3ef90a5a186496ebb61d933699f03a9a1768b0de
gominnam <kosok03@naver.com>
2024-08-23 19:52:37 +0900

    add symmetric tree solving easy problem

---
+++ src/LeetCode/BinaryTree/easy/SymmetricTree.java
@@ -0,0 +1,110 @@
+package LeetCode.BinaryTree.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SymmetricTree {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public boolean isSymmetric(TreeNode root) {
+        List<Integer> leftResult = new ArrayList<>();
+        List<Integer> rightResult = new ArrayList<>();
+        leftTree(root.left, leftResult);
+        rightTree(root.right, rightResult);
+        return isSame(leftResult, rightResult);
+    }
+
+
+    public void leftTree(TreeNode tree, List<Integer> result){
+        if(tree == null){
+            result.add(null);
+            return;
+        }
+        result.add(tree.val);
+        leftTree(tree.left, result);
+        leftTree(tree.right, result);
+    }
+
+    public void rightTree(TreeNode tree, List<Integer> result){
+        if(tree == null){
+            result.add(null);
+            return;
+        }
+        result.add(tree.val);
+        rightTree(tree.right, result);
+        rightTree(tree.left, result);
+    }
+
+    public boolean isSame(List<Integer> left, List<Integer> right){
+        if(left.size() != right.size()) return false;
+        for(int i=0; i<left.size(); i++){
+            if(left.get(i) != right.get(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        SymmetricTree st = new SymmetricTree();
+        TreeNode root = st.new TreeNode(1);
+        root.left = st.new TreeNode(2);
+        root.right = st.new TreeNode(2);
+        root.left.left = st.new TreeNode(3);
+        root.left.right = st.new TreeNode(4);
+        root.right.left = st.new TreeNode(4);
+        root.right.right = st.new TreeNode(3);
+        System.out.println(st.isSymmetric(root));
+    }
+}
+
+/*
+
+1)
+Thinking:
+1)
+- Symmetric Tree 란 Root-Tree 기준으로 left-Tree 와 right-Tree 가 대칭인 Tree
+- 그러므로 왼쪽 트리는 left 부터 오른쪽 트리는 right 부터 탐색하면서 값을 비교하여 해결
+
+2) 우아한 해답지 소스코드
+    ex) 1
+        / \
+       2   2
+      / \ / \
+     3  4 4  3
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    // Recursive
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        if (t1.val != t2.val) {
+            return false;
+        }
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+
+-ref: https://leetcode.com/problems/symmetric-tree/
+
+ */
