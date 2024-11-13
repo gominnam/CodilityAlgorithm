111a2934c5eededc39267e9022000f096649e8f4
gominnam <kosok03@naver.com>
2024-11-14 00:27:44 +0900

    refactoring rotten orange bfs template

--- src/LeetCode/BFS/medium/RottingOranges.java
+++ src/LeetCode/BFS/medium/RottingOranges.java
@@ -5,81 +5,65 @@ import java.util.Queue;
 
 public class RottingOranges {
 
-    static final int[][] mv = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    static class Node{
+    static class Point{
         int x;
         int y;
 
-        public Node(int x, int y){
+        public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
 
     public int orangesRotting(int[][] grid) {
-        boolean isRotten = false;
-        boolean isFresh = false;
-        Queue<Node> rottenPos = new LinkedList<>();
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    isFresh = true;
-                }
-                if(grid[i][j] == 2){
-                    rottenPos.add(new Node(i, j));
-                    isRotten = true;
+        Queue<Point> queue = new LinkedList<>();
+        int freshOranges = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new Point(i, j));
+                } else if (grid[i][j] == 1) {
+                    freshOranges++;
                 }
             }
         }
 
-        if(!isRotten) {
-            if(isFresh) return -1;
-            return 0;
-        }
+        if (freshOranges == 0) return 0;
 
-        int result = bfs(grid, rottenPos);
+        int minutes = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean hasRotten = false;
 
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 1) return -1;
-            }
-        }
+            for (int i = 0; i < size; i++) {
+                Point point = queue.poll();
+                for (int[] direction : DIRECTIONS) {
+                    int nx = point.x + direction[0];
+                    int ny = point.y + direction[1];
 
-        return result;
-    }
-
-    public int bfs(int[][] grid, Queue<Node> rQueue){
-        int result = 0;
-
-        while (!rQueue.isEmpty()) {
-            int size = rQueue.size();
-
-            boolean flag = false;
-            for(int i=0; i<size; i++){
-                Node curNode = rQueue.poll();
-                for(int j=0; j<4; j++){
-                    int nx = curNode.x + mv[j][0];
-                    int ny = curNode.y + mv[j][1];
-                    if(nx < 0 || ny < 0 || nx >= grid.length || ny >= grid[0].length) continue;
-                    if(grid[nx][ny] != 1) continue;
-                    rQueue.add(new Node(nx, ny));
-                    grid[nx][ny] = 2;
-                    flag = true;
+                    if (nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        queue.add(new Point(nx, ny));
+                        freshOranges--;
+                        hasRotten = true;
+                    }
                 }
             }
 
-            if(flag)result++;
+            if (hasRotten) minutes++;
         }
 
-        return result;
+        return freshOranges == 0 ? minutes : -1;
     }
 
     public static void main(String[] args){
         RottingOranges ro = new RottingOranges();
-//        int[][] grid = {{2,1,1},{1,1,0},{0,1,1}};
+        int[][] grid = {{2,1,1},{1,1,0},{0,1,1}};
 //        int[][] grid = {{2,1,1},{0,1,1},{1,0,1}};
-        int[][] grid = {{0,2}};
+//        int[][] grid = {{0,2}};
         System.out.println(ro.orangesRotting(grid));
     }
 }
