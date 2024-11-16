f1fe37acc6de1d1746be660810eedac074d83e01
gominnam <kosok03@naver.com>
2024-11-16 23:02:18 +0900

    modify trie performance improvement (hashtable)

--- src/LeetCode/HashTable/medium/ImplementTrie.java
+++ src/LeetCode/HashTable/medium/ImplementTrie.java
@@ -5,12 +5,59 @@ import java.util.Map;
 
 public class ImplementTrie {
 
+//    class TrieNode {
+//        Map<Character, TrieNode> children;
+//        boolean isEndOfWord;
+//
+//        public TrieNode() {
+//            children = new HashMap<>();
+//            isEndOfWord = false;
+//        }
+//    }
+//
+//    private TrieNode root;
+//
+//    public ImplementTrie() {
+//        root = new TrieNode();
+//    }
+//
+//    public void insert(String word) {
+//        TrieNode node = root;
+//        for(char c : word.toCharArray()){
+//            node.children.putIfAbsent(c, new TrieNode());
+//            node = node.children.get(c);
+//        }
+//        node.isEndOfWord = true;
+//    }
+//
+//    public boolean search(String word) {
+//        TrieNode node = root;
+//        for(char c : word.toCharArray()){
+//            node = node.children.get(c);
+//            if(node == null){
+//                return false;
+//            }
+//        }
+//        return node.isEndOfWord;
+//    }
+//
+//    public boolean startsWith(String prefix) {
+//        TrieNode node = root;
+//        for(char c : prefix.toCharArray()){
+//            node = node.children.get(c);
+//            if(node == null){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
     class TrieNode {
-        Map<Character, TrieNode> children;
+        TrieNode[] children;
         boolean isEndOfWord;
 
         public TrieNode() {
-            children = new HashMap<>();
+            children = new TrieNode[26]; // 알파벳 소문자만 다룰 경우
             isEndOfWord = false;
         }
     }
@@ -23,18 +70,22 @@ public class ImplementTrie {
 
     public void insert(String word) {
         TrieNode node = root;
-        for(char c : word.toCharArray()){
-            node.children.putIfAbsent(c, new TrieNode());
-            node = node.children.get(c);
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
         }
         node.isEndOfWord = true;
     }
 
     public boolean search(String word) {
         TrieNode node = root;
-        for(char c : word.toCharArray()){
-            node = node.children.get(c);
-            if(node == null){
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            node = node.children[index];
+            if (node == null) {
                 return false;
             }
         }
@@ -43,9 +94,10 @@ public class ImplementTrie {
 
     public boolean startsWith(String prefix) {
         TrieNode node = root;
-        for(char c : prefix.toCharArray()){
-            node = node.children.get(c);
-            if(node == null){
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            node = node.children[index];
+            if (node == null) {
                 return false;
             }
         }
@@ -64,6 +116,9 @@ public class ImplementTrie {
 }
 
 /*
+
+Thinking:
+: HashTable 자료구조처럼 Array를 사용하여 최적화를 구현. // Map -> Array
 
 -ref: https://leetcode.com/problems/implement-trie-prefix-tree/description/
 
