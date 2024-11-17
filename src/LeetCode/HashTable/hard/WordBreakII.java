fb6d8a7f36bb770f091f7a5ccb71c74f0eba743b
gominnam <kosok03@naver.com>
2024-11-17 21:06:03 +0900

    hashtable word break problem

---
+++ src/LeetCode/HashTable/hard/WordBreakII.java
@@ -0,0 +1,50 @@
+package LeetCode.HashTable.hard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordBreakII {
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return dfs(s, wordDict, new HashMap<>(), 0);
+    }
+
+    private List<String> dfs(String s, List<String> wordDict, Map<Integer, List<String>> memo, int start) {
+        if (memo.containsKey(start)) {
+            return memo.get(start);
+        }
+
+        List<String> result = new ArrayList<>();
+        if (start == s.length()) {
+            result.add("");
+            return result;
+        }
+
+        for (String word : wordDict) {
+            if (s.startsWith(word, start)) { // start 위치부터 시작하는 부분이 word와 같은지 확인
+                List<String> sublist = dfs(s, wordDict, memo, start + word.length());
+                for (String sub : sublist) {
+                    result.add(word + (sub.isEmpty() ? "" : " ") + sub);
+                }
+            }
+        }
+
+        memo.put(start, result);
+        return result;
+    }
+
+    public static void main(String[] args){
+        WordBreakII wb = new WordBreakII();
+        String s = "catsanddog";
+        List<String> wordDict = List.of("cat", "cats", "and", "sand", "dog");
+        System.out.println(wb.wordBreak(s, wordDict)); // ["cats and dog", "cat sand dog"]
+    }
+}
+
+/*
+
+-ref: https://leetcode.com/problems/word-break-ii/
+
+ */
