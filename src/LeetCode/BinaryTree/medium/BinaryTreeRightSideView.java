2a4ef9faa43a581dea1bd531804f92340eddd743
gominnam <kosok03@naver.com>
2024-11-19 19:25:17 +0900

    performance improvement in one way first right.node searching

--- src/LeetCode/BinaryTree/medium/BinaryTreeRightSideView.java
+++ src/LeetCode/BinaryTree/medium/BinaryTreeRightSideView.java
@@ -24,24 +24,17 @@ public class BinaryTreeRightSideView {
         List<Integer> result = new ArrayList<>();
         dfs(root, result,  0);
         return result;
-    } //queue 이용 ?
+    }
 
-    public void dfs(TreeNode node, List<Integer> result, int depth){
-        if(node == null) return;
+    public void dfs(TreeNode node, List<Integer> result, int depth) {
+        if (node == null) return;
 
-        if(depth == result.size()){
+        if (depth == result.size()) {
             result.add(node.val);
         }
-        else {
-            result.set(depth, node.val);
-        }
 
-        if(node.left != null) {
-            dfs(node.left, result, depth+1);
-        }
-        if(node.right != null) {
-            dfs(node.right, result, depth+1);
-        }
+        dfs(node.right, result, depth + 1);
+        dfs(node.left, result, depth + 1);
     }
 
     public static void main(String[] args){
@@ -64,6 +57,14 @@ public class BinaryTreeRightSideView {
 }
 
 /*
+
+Thinking:
+
+1) queue 를 사용해여 해결해도 될거 같다. 메모리는 더 크게 먹지만 읽기는 좋을 것.
+
+2) 자식노드를 왼쪽부터 탐색하는 것으로 풀었지만
+   오른쪽부터 탐색하고 이미 index에 값이 있으면 pass 하는 형식으로 수정하여 속도 개선
+
 
 -ref: https://leetcode.com/problems/binary-tree-right-side-view/description/
 
