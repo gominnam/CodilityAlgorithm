9f3c65f77fc978365f369a97d1c5b3e635a9ca8e
gominnam <kosok03@naver.com>
2025-01-27 18:40:18 +0900

    refactoring backtarcking memory efficiency by reusing board array

--- src/LeetCode/BackTracking/medium/WordSearch.java
+++ src/LeetCode/BackTracking/medium/WordSearch.java
@@ -2,38 +2,37 @@ package LeetCode.BackTracking.medium;
 
 public class WordSearch {
 
-    boolean[][] cache;
     int[][] mv = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
 
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                boolean isTrue = false;
-                if(board[i][j] == word.charAt(0)){
-                    cache = new boolean[m][n]; // current position
-                    int[] cur = {i, j};
-                    cache[i][j] = true;
-                    isTrue = backTracking(board, word, 1, cur);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0) && backtracking(board, word, 0, i, j)) {
+                    return true;
                 }
-                if(isTrue) return true;
             }
         }
-
         return false;
     }
 
-    public boolean backTracking(char[][] board, String word, int depth, int[] current){
-        if(depth == word.length()) return true;
-        for(int i=0; i<4; i++){
-            int nx = current[0] + mv[i][0];
-            int ny = current[1] + mv[i][1];
-            if(nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length || cache[nx][ny] || board[nx][ny] != word.charAt(depth)) continue;
-            cache[nx][ny] = true;
-            if(backTracking(board, word, depth+1, new int[] {nx, ny})) return true;
-            cache[nx][ny] = false;
+    private boolean backtracking(char[][] board, String word, int depth, int x, int y) {
+        if (depth == word.length()) return true;
+
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length || board[x][y] != word.charAt(depth)) return false;
+
+        char temp = board[x][y];
+        board[x][y] = '#';
+
+        for (int[] move : mv) {
+            int nx = x + move[0];
+            int ny = y + move[1];
+            if (backtracking(board, word, depth + 1, nx, ny)) return true;
         }
+
+        board[x][y] = temp;
         return false;
     }
 
@@ -45,8 +44,8 @@ public class WordSearch {
                 {'A', 'D', 'E', 'E'}};
 
         System.out.println(wordSearch.exist(board, "ABCCED")); // true
-        System.out.println(wordSearch.exist(board, "SEE")); // true
-        System.out.println(wordSearch.exist(board, "ABCB")); // false
+//        System.out.println(wordSearch.exist(board, "SEE")); // true
+//        System.out.println(wordSearch.exist(board, "ABCB")); // false
     }
 }
 
@@ -54,6 +53,7 @@ public class WordSearch {
 
 Thinking:
 - backTracking 문제로 해결
+- refactoring 처음에는 boolean[][] cache 메모리를 사용 했지만, board를 활용하여 메모리 최적화
 
 
 -ref: https://leetcode.com/problems/word-search/
