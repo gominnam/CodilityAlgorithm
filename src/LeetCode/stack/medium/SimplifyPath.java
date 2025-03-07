298e12a82158d06b9c3cf1eee0a9ccaba54f3de8
gominnam <kosok03@naver.com>
2025-03-07 15:11:56 +0900

    Add: directory 파싱문제 Deque 자료구조를 사용하여 간결하게 해결 가능 메서드 추가

--- src/LeetCode/stack/medium/SimplifyPath.java
+++ src/LeetCode/stack/medium/SimplifyPath.java
@@ -1,5 +1,7 @@
 package LeetCode.stack.medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class SimplifyPath {
@@ -30,20 +32,46 @@ public class SimplifyPath {
         return sb.toString();
     }
 
+    public String simplifyPath_Deque(String path){
+        Deque<String> stack = new ArrayDeque<>();
+        String[] paths = path.split("/");
+
+        for (String current : paths) {
+            if (current.isEmpty() || current.equals(".")) {
+                continue;
+            }
+            if (current.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(current);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, '/').insert(1, stack.pop());
+        }
+
+        return !sb.isEmpty() ? sb.toString() : "/";
+    }
+
     public static void main(String[] args) {
         SimplifyPath o = new SimplifyPath();
         System.out.println(o.simplifyPath("/home/"));//"/home"
         System.out.println(o.simplifyPath("/../"));//"/"
         System.out.println(o.simplifyPath("/home//foo/"));//"/home/foo"
         System.out.println(o.simplifyPath("/a/./b/../../c/"));//"/c"
+        System.out.println(o.simplifyPath_Deque("/a/../../b/../c//.//"));//"/c"
     }
 }
 
 /*
 
 Thinking:
-- Stack 자료구조를 이용하여 뒤에서부터 경로 탐색, 추가
-- 보다 나은 자료구조 Deque를 사용하면 더 좋을 듯
+- Stack 자료구조를 이용하여 뒤에서부터 경로 탐색, 추가하는 방식으로 해결
+- simplifyPath_Deque 메서드에서 자료구조 Deque 를 사용하면 더 간결하게 구현 가능
 
 -ref: https://leetcode.com/problems/simplify-path
 
