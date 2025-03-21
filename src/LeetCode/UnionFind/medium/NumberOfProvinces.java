4fe2efe558393c7ce11af07124e4d0756d332a81
gominnam <kosok03@naver.com>
2025-03-21 12:53:07 +0900

    refactor: UnionFind 대신 dfs를 사용하여 시간복잡도, 메모리 최적화 방법 추가

--- src/LeetCode/UnionFind/medium/NumberOfProvinces.java
+++ src/LeetCode/UnionFind/medium/NumberOfProvinces.java
@@ -25,7 +25,7 @@ public class NumberOfProvinces {
 
         Set<Integer> connectedSet = new HashSet<>();
         for(int i=0; i<n; i++){
-            connectedSet.add(getParent(parents[i]));
+            connectedSet.add(getParent(i));
         }
 
         return connectedSet.size();
@@ -43,6 +43,32 @@ public class NumberOfProvinces {
         else parents[a] = b;
     }
 
+    public int findCircleNum_dfs(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, i);
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int from) {
+        visited[from] = true;
+
+        for (int i = 0; i < isConnected[from].length; i++) {
+            if (from != i && isConnected[from][i] == 1 && !visited[i]) {
+                dfs(isConnected, visited, i);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         NumberOfProvinces n = new NumberOfProvinces();
         int[][] isConnected = new int[][]{{1,1,0},{1,1,0},{0,0,1}};
@@ -53,7 +79,8 @@ public class NumberOfProvinces {
 /*
 
 Thinking:
-- unionFind와 Set 자료구조를 통해서 연결된 도시의 수를 구하는 문제와 같은 유형
+- unionFind와 Set 자료구조를 통해서 연결된 도시의 수를 구하는 문제와 같은 유형(시간복잡도 O(N^2))
+- finCircleNum_dfs는 dfs를 통해서 시간복잡도와 메모리 최적화 (시간복잡도 O(N + E))
 
 
 - ref: https://leetcode.com/problems/number-of-provinces/
